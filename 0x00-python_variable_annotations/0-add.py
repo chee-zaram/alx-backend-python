444b62e5c34b80e700439286575b68426a517340
Emmanuel Chee-zaram Okeke <ecokeke21@gmail.com>
2023-07-06 11:02:02 +0100

    feat(0x00): define function to add two floats and return the result

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module contains the type-annotated `add` function
+"""
+
+
+def add(a: float, b: float) -> float:
+    """This function returns the sum of two floats, `a` and `b`"""
+    return a + b
