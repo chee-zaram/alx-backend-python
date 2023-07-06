ca1aa80232f225300d7876848ee4dcfac52dcbfb
Emmanuel Chee-zaram Okeke <ecokeke21@gmail.com>
2023-07-06 11:19:51 +0100

    feat(0x00): define function to concatenate two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module defines the type-annotated function `concat`
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """This function concatenates two strings and returns the result"""
+    return str1 + str2
