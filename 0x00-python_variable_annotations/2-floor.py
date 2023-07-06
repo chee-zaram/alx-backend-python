2cc81562b4b446d34f5a74962284b47bf273d4a3
Emmanuel Chee-zaram Okeke <ecokeke21@gmail.com>
2023-07-06 11:34:56 +0100

    feat(0x00): define function to return floor of a float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module defines a function `floor`
+"""
+
+
+def floor(n: float) -> int:
+    """This function takes an argument `n` and returns the floor value"""
+    return int(n)
